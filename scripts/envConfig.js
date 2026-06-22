99e362b87a16aa4c623cf2d535c9bf509e22eba1
rohitamazon550 <rohitamazon550@gmail.com>
2026-06-22 14:04:38 +0530

    Update envConfig.js

--- scripts/envConfig.js
+++ scripts/envConfig.js
@@ -4,7 +4,7 @@
 
 window.envConfig = {
   almAuthEndpoint: "https://learningmanager.adobe.com/oauth/o/authorize",
-  adobeIOAlmEndpoint: "https://1070326-edsalmintegration-stage.adobeio-static.net/api/v1/web/EdsAlm/authentication",
+  adobeIOAlmEndpoint: "https://1070326-edsalmintegration-stage.adobeio-static.net/api/v1/web/appbuilder/authentication",
   almClientId: "62f33554-103c-4fcb-b68c-d35c1d3da6a5",
   almAccount: "121816"
 };
